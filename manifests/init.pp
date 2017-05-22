e1eacf3d62cf936232400307aa22765d0c1ca4a1
Jordi Prats <jprats@systemadmin.es>
2017-05-22 10:59:21 +0200

    lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 class openscap(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits openscap::params{
+                $manage_package        = true,
+                $package_ensure        = 'installed',
+                $manage_service        = true,
+                $manage_docker_service = true,
+                $service_ensure        = 'running',
+                $service_enable        = true,
+              ) inherits openscap::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
 
