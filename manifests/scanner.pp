17d434c36d8a9a505b24075b91d1a85de2aa5e42
Jordi Prats <jprats@systemadmin.es>
2017-05-25 13:19:18 +0200

    lint

--- manifests/scanner.pp
+++ manifests/scanner.pp
@@ -1,11 +1,7 @@
 class openscap::scanner (
-                          $manage_package        = true,
-                          $package_ensure        = 'installed',
-                          $manage_service        = true,
-                          $manage_docker_service = true,
-                          $service_ensure        = 'running',
-                          $service_enable        = true,
-                          $basedir               = '/opt/openscap',
+                          $manage_package = true,
+                          $package_ensure = 'installed',
+                          $basedir        = '/opt/openscap',
                         ) inherits openscap::params{
 
   validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
