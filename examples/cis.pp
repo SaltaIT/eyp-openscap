bece6bee8aa9006888e2d48c3922ae5b64231c43
Jordi Prats <jordi.prats@gmail.com>
2019-05-24 15:12:07 +0200

    demo

--- examples/cis.pp
+++ examples/cis.pp
@@ -2,16 +2,26 @@
 # classes:
 #   - openscap::scanner
 # openscapscannerxccdfs:
-#   'scytl':
-#     profile: 'scytl_basic_security_profile'
+#   'demo':
+#     profile: 'demo_basic_security_profile'
 #     hour: '14'
 #     minute: '40'
 # openscapscannerprofiles:
-#   'scytl_basic_security_profile':
-#     source: 'puppet:///customers/MORPHO-KENYA_7787c3ee034e/scytl_basic_security_profile.xml'
+#   'demo_basic_security_profile':
+#     source: 'puppet:///customers/MORPHO-KENYA_7787c3ee034e/demo_basic_security_profile.xml'
 
 class { 'openscap::scanner':
   basedir_mode => '0755',
   results_mode => '0755',
   reports_mode => '0755',
+}
+
+openscap::scanner::xccdf { 'demo':
+  profile => 'demo_basic_security_profile',
+  hour    => '0',
+  minute  => '0',
+}
+
+openscap::scanner::profile { 'demo_basic_security_profile':
+  source => file('openscap/demo.xml'),
 }
